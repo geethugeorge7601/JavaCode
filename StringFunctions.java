fcb7d079f23802d790707287be51b6dab516d92b
geethugeorge7601 <mail4geethugeorge@gmail.com>
2022-12-07 10:38:46 +0530

    modified string functions

--- StringFunctions.java
+++ StringFunctions.java
@@ -27,6 +27,7 @@ class StringFunctions
 	System.out.println("String builder append "+sb.append(" Guys"));
 	
 	System.out.println("String builder reverse "+sb.reverse());
+	System.out.println();
 	
 	}
 }
